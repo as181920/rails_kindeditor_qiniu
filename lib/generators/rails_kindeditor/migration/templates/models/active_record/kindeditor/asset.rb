ba62a27fb2d279192215a11f3e022e7e4dde4200
Andersen Fan <as181920@hotmail.com>
2013-01-28 13:53:57 +0800

    change generator template model:asset.rb

--- lib/generators/rails_kindeditor/migration/templates/models/active_record/kindeditor/asset.rb
+++ lib/generators/rails_kindeditor/migration/templates/models/active_record/kindeditor/asset.rb
@@ -1,13 +1,13 @@
 class Kindeditor::Asset < ActiveRecord::Base
   self.table_name = 'kindeditor_assets'
-  mount_uploader :asset, AssetUploader
-  validates_presence_of :asset
-  before_save :update_asset_attributes
-  attr_accessible :asset
-  
-  private
-  def update_asset_attributes
-    self.file_size = asset.file.size
-    self.file_type = asset.file.content_type
-  end
+  attr_accessible :asset, :file_name, :file_type, :file_size
+  #mount_uploader :asset, AssetUploader
+  #validates_presence_of :asset
+  #before_save :update_asset_attributes
+  #
+  #private
+  #def update_asset_attributes
+  #  self.file_size = asset.file.size
+  #  self.file_type = asset.file.content_type
+  #end
 end
